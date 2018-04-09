451d0d86bc99eba7359bcfe789c5b61a762eef53
Marina Vasilevsky <marina.vasilevsky@tytocare.com>
2018-04-09 12:01:15 +0300

    Add comments how to create the input

--- hello.cc
+++ hello.cc
@@ -9,16 +9,25 @@
 int main(int argc, char** argv) {
     seastar::app_template app;
 	 return app.run(argc, argv, [] {
-		 test_fragments *pTest = new test_fragments();
-		 return pTest->CreateFiles(5, RECORD_SIZE*4).then([pTest] (std::vector<sstring> results) {
+		 test_fragments *pTest = new test_fragments(); // Unit test that creates files
+		 return pTest->CreateFiles(5, RECORD_SIZE*4).then([pTest] (std::vector<sstring> results) { // Creates different files for testing,
+																																 // I use only the first file
+																																 // The parameter to the function is number of test files and size of every file
+																																 // I used this code also to test read write on my pc
+																																 // If needed, I can pass any of as command line parameters application arguments
+																																 // Please note I used hard coded /tmp directory
+																																 // All parameters - source, destination directories, size of fragment, memory limitation
+																																 // Should be configurable, in real life I would pass json configuration file as program input argument
 			 sort_fragments *SortF = new sort_fragments();
-			 return SortF->CreateFragments(RECORD_SIZE*2, results[0], "/tmp").then ([results] (std::vector<sstring> fragments) {
+			 return SortF->CreateFragments(RECORD_SIZE*2, results[0], "/tmp").then ([results] (std::vector<sstring> fragments) { // This is step 1 of the sorting - fragmentation
+																																										// If previous step created more files, I am using only the first created file
+																																										// The first parameter is size of fragment, second is input file
 
 				 //delete pTest;
 				 std::cout << "Hello world " << fragments.size() << std::endl;
-
 				 merge_files *mf = new merge_files();
-				 return mf->Process(fragments, RECORD_SIZE).then([garbage = std::move(mf)] () {
+				 return mf->Process(fragments, RECORD_SIZE).then([garbage = std::move(mf)] () {       // Step 2 : merge fragments and create single output file
+
 					 std::cout << "The end " << std::endl;
 					 delete garbage;
 					 return make_ready_future<>();
@@ -31,81 +40,3 @@ int main(int argc, char** argv) {
 
 
 
-/*
- *
- *
-#include <vector>
-#include <boost/range/algorithm.hpp>
-
-std::vector<std::string> stringarray;
-boost::sort(stringarray);
- *
- */
-
-#if 0
-#include <iostream>
-#include <queue>
-using namespace std;
-
-class Record
-{
-private:
-	 std::string FileName;
-	 std::string FirstRecord;
-
-public:
-	 Record( std::string FileName,
-				std::string FirstRecord, )  : value( v ) {}
-	 int Val() const { return value; }
-};
-
-struct CompareNode : public std::binary_function<Node*, Node*, bool>
-{
-  bool operator()(const Node* lhs, const Node* rhs) const
-  {
-	  return lhs->Val() < rhs->Val();
-  }
-};
-
-int main()
-{
-	 priority_queue<Node*,vector<Node*>, CompareNode > pq;
-
-	 pq.push( new Node( 111 ) );
-	 pq.push( new Node( 1111 ) );
-	 pq.push( new Node( 1011 ) );
-	 pq.push( new Node( 100 ) );
-	 pq.push( new Node( 1110 ) );
-	 pq.push( new Node( 101 ) );
-
-	 while ( !pq.empty() )
-	 {
-		  Node* n = pq.top();
-		  cout << n->Val() << endl;
-		  pq.pop();
-
-		  // Delete pointer that vector contains
-		  delete n;
-	 }
-	 cin.get();
-}
-
-template <typename PriorityQueue>
-void basic_interface(void)
-{
-	 PriorityQueue pq;
-
-	 pq.push(2);
-	 pq.push(3);
-	 pq.push(1);
-
-	 cout << "Priority Queue: popped elements" << endl;
-	 cout << pq.top() << " "; // 3
-	 pq.pop();
-	 cout << pq.top() << " "; // 2
-	 pq.pop();
-	 cout << pq.top() << " "; // 1
-	 pq.pop();
-	 cout << endl;
-}
-#endif
